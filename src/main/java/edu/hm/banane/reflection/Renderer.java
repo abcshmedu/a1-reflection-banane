607afa3b80cce9d7714fb1be17f6a347ff642514
TARTARUSPC\TartaPc <pmasch@hm.edu>
2017-03-26 09:34:45 +0200

    Renderer Code + Funktionierender Test

--- src/main/java/edu/hm/banane/reflection/Renderer.java
+++ src/main/java/edu/hm/banane/reflection/Renderer.java
@@ -1,12 +1,53 @@
 package edu.hm.banane.reflection;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+
 public class Renderer {
+    private Object o;
 
 	public Renderer(Object object) {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public String render() {
-		return null;
-	}
+        this.o = object;
+    }
+
+    public String render() {
+        Class<?> c = o.getClass();
+        Field[] fields = c.getDeclaredFields();
+        final StringBuilder strb = new StringBuilder(256);
+        strb.append("Instance of ")
+                .append(c.getName())
+                .append(":\n");
+        for (Field field : fields) {
+            if (field.getAnnotation(RenderMe.class) != null) {
+                strb.append(field.getName())
+                        .append(" (Type ");
+                if (field.getType().isArray()) {
+                    strb.append(field.getType().getComponentType())
+                            .append("[]");
+                } else {
+                    strb.append(field.getType().getName());
+                }
+                strb.append("): ");
+                try {
+                    field.setAccessible(true);
+                    Object obj = field.get(o);
+                    if (field.getType().isArray()) {
+                        int length = Array.getLength(obj);
+                        strb.append("[");
+                        for (int i = 0; i < length; i++) {
+                            strb.append(Array.get(obj, i))
+                                    .append(", ");
+                        }
+                        strb.append("]");
+                    } else {
+                        strb.append(obj);
+                    }
+                    strb.append("\n");
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return strb.toString();
+    }
 }
